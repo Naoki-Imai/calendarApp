ae1b54370b5bc267253717caf751d372bb7c8839
EC2 Default User <ec2-user@ip-172-31-12-187.ap-northeast-1.compute.internal>
2020-09-26 04:00:31 +0000

    カレンダー機能追加

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -1,23 +1,3 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
-// vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file. JavaScript code in this file should be added after the last require_* statement.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
-//= require jquery
-//= require moment
-//= require fullcalendar
-//= require rails-ujs
-//= require activestorage
-//= require turbolinks
-//= require_tree .
-
 
 $(function () {
   $(document).on('turbolinks:load', function () {
@@ -32,6 +12,7 @@ $(function () {
         eventCalendar();
       });
       $(document).on('turbolinks:before-cache', clearCalendar);
+
       $('#calendar').fullCalendar({
         events: '/events.json',
         titleFormat: 'YYYY年 M月',
@@ -58,7 +39,7 @@ $(function () {
         //イベントの時間表示を２４時間に
         timeFormat: "HH:mm",
         //イベントの色を変える
-        eventColor: '#87cefa',
+        eventColor: '#63ceef',
         //イベントの文字色を変える
         eventTextColor: '#000000',
       });
